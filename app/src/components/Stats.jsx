86c421d8ce465193404d82490cfbdfb359cef9e0
lynh tran <tran0879@umn.edu>
2021-01-23 18:13:21 -0600

    Changed image of bucket to gallon jug

--- app/src/components/Stats.jsx
+++ app/src/components/Stats.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import Button from 'react-bootstrap/Button';
 import Image from 'react-bootstrap/Image';
-import bucket from './bucket.png';
+import gallon from './gallon.png';
 import Accordion from 'react-bootstrap/Accordion';
 import Card from 'react-bootstrap/Card';
 
@@ -32,7 +32,7 @@ class Stats extends Component {
         return ( 
           <div>
             {[...Array(gallons),].map((e,i) =>{ 
-              return <Image src= {bucket} fluid width = "200" height ="300"/>})}
+              return <Image src= {gallon} fluid width = "200" height ="300"/>})}
             <div style ={title}> Total Water Usage is {this.state.gallons} Gallons </div>
             <h5> Select option below to view water usage for specific appliance: </h5>
             <div>
@@ -48,7 +48,7 @@ class Stats extends Component {
                     <Accordion.Collapse eventKey={i}>
                         <Card.Body>
                             {[...Array(this.state.i),].map((e, i) =>{ 
-                                return <Image src= {bucket} fluid width = "200" height ="300"/>})}
+                                return <Image src= {gallon} fluid width = "200" height ="300"/>})}
                         </Card.Body>
                     </Accordion.Collapse>
                     </Card>
