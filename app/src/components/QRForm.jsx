8671b54962e0eb74bece1f96059184f782c98cf5
Evan Tredal <treda004@umn.edu>
2021-01-23 21:55:57 -0500

    Functions for QR

--- app/src/components/QRForm.jsx
+++ app/src/components/QRForm.jsx
@@ -9,24 +9,41 @@ class QR extends Component {
         this.state={
             url: "http://google.com",
             id: "Evan",
-            Facility: "Kitchen",
-            Gallons: "2"
+            facility: "",
+            gallons: ""
         };
+
+        this.handleFacilityChange = this.handleFacilityChange.bind(this);
+        this.handleGallonsChange = this.handleGallonsChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+    handleFacilityChange(event) {
+        this.setState({facility: event.target.value});
+    }
+
+    handleGallonsChange(event) {
+        this.setState({gallons: event.target.value});
+    }
+
+    handleSubmit(event) {
+        alert('A name was submitted: ' + this.state.facility);
+        event.preventDefault();
     }
 
     render() {
         return (
-            <Form as='form' id='rs'>
+            <Form as='form' id='rs' onSubmit={this.handleSubmit}>
                 <Form.Group controlId="appliance_id">
                     <Form.Label>Choose a name for your appliance</Form.Label>
-                    <Form.Control type="text" placeholder="Kitchen Faucet" required/>
+                    <Form.Control type="text" value={this.state.facility} onChange={this.handleFacilityChange} required/>
                 </Form.Group>
                 <Form.Group controlId="gallons_total">
                     <Form.Label>How many gallons in a single use?</Form.Label>
-                    <Form.Control type="text" placeholder="2" required/>
+                    <Form.Control type="text" value={this.state.gallons} onChange={this.handleGallonsChange} required/>
                 </Form.Group>
                 <Button type="submit">
-                    <Link to={'http://www.api.qrserver.com/v1/create-qr-code/?data=${this.state.url}?UserID=${this.state.id}%26FacilityID=${this.state.facility}%26Gallons=${this.state.gallons}&size=200x200'}>
+                    <Link href={`http://www.api.qrserver.com/v1/create-qr-code/?data=${this.state.url}?UserID=${this.state.id}%26FacilityID=${this.state.facility}%26Gallons=${this.state.gallons}&size=200x200`}>
                         Get QR Code
                     </Link>
                 </Button>
