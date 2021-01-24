c2301407a0597ba76f9c996d9a18c15105bfb12b
Your Name <nathanielbd@gmail.com>
2021-01-23 21:08:14 -0600

    removed extra add app button

--- app/src/components/Stats.jsx
+++ app/src/components/Stats.jsx
@@ -71,7 +71,6 @@ class Stats extends Component {
             )})}
         </Accordion>
         </div>
-            <Button href={`${process.env.PUBLIC_URL}/2`} style ={styles}>Add Appliance</Button>
           </div> );
     }
 }
